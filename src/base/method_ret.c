99f07df7d57c7efd125f6a3ad710a7c55e0cb808
toitate <moonitate@gmail.com>
2011-10-17 06:22:11 +0000

    update method file

---
+++ src/base/method_ret.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+
+struct structure {
+	int id;
+	char name[10];
+};
+
+
+int integer();
+char character();
+struct structure structure_ret();
+
+int *integer_p();
+char *character_p();
+struct structure *structure_ret_p();
+
+int
+main() {
+
+	int a = integer();
+	printf("%d %p\n\n", a, &a);
+
+	char c = character();
+	printf("%c %p\n\n", c, &c);
+
+	struct structure str = structure_ret();
+	printf("%d %s %d %p\n\n", str.id, str.name, sizeof(str), &str);
+
+	printf("\n\n");
+
+
+	int *i = integer_p();
+	printf("%d %p\n\n", *i, i);
+
+	char *d = character_p();
+	printf("%c %p\n\n", *d, d);
+
+	struct structure *str_p = structure_ret_p();
+	//TODO array within structure
+	printf("%d %s %d %p\n\n", str_p->id, str_p->name, sizeof(str_p), str_p);
+	
+	return 0;
+}
+
+
+int integer() {
+	int a = 9;
+
+	printf("%d %p\n", a, &a);
+	return a;
+}
+
+char character() {
+	char c = 'c';
+
+	printf("%c %p\n", c, &c);
+	return c;
+}
+
+struct structure structure_ret() {
+
+	struct structure str = {99, "oitate"};
+	printf("%d %s %d %p\n", str.id, str.name, sizeof(str), &str);
+	return str;
+}
+
+
+
+int *integer_p() {
+	int temp = 9;
+	int *a = &temp;
+
+	printf("%d %p\n", *a, a);
+	return a;
+}
+
+char *character_p() {
+	char temp = 'c';
+	char *c = &temp;
+
+	printf("%c %p\n", *c, c);
+	return c;
+}
+
+struct structure *structure_ret_p() {
+
+	struct structure temp = {99, "oitate"};
+	struct structure *str = &temp;
+	printf("%d %s %d %p\n", str->id, str->name, sizeof(str), &str);
+	return str;
+}
