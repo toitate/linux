cb0c16731175e10b0b50335550244121e616088b
toitate <moonitate@gmail.com>
2011-10-19 00:55:27 +0000

    update driver

---
+++ src/driver/hello.c
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void) {
+	printk(KERN_ALERT "driver loaded\n");
+	return 0;
+}
+
+static void hello_exit(void) {
+	printk(KERN_ALERT "driver unloaded\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
